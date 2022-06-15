b527668b6d850589980bb329bd369940b6dc1273
Stabowleo <minegineer71@gmail.com>
2022-06-15 10:18:55 +0100

    made a change to a file

--- sprites.py
+++ sprites.py
@@ -5,4 +5,5 @@ class BodyPiece(pygame.sprite.Sprite):
 
     def __init__(self):
         super().__init__()
-        self.image = None
+        self.image = pygame.image.load("art/SnakeBody.png").convert_alpha()
+        self.rect = self.image.get_rect()

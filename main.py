2dde3f8df347ee116cdc12bdd60c857f3b9e1292
Stabowleo <minegineer71@gmail.com>
2022-06-15 09:59:12 +0100

    H

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import pygame
 from pygame.locals import *
+
+
+screen = pygame.display.set_mode((480, 320))
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()

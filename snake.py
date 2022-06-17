431cca3d3f4d424b7ef4999dca2a6d3754abfa2f
Stabowleo <minegineer71@gmail.com>
2022-06-17 21:37:51 +0100

    Created the game over screen and changed way game works

--- snake.py
+++ snake.py
@@ -1,9 +1,8 @@
 import random
-from tkinter.tix import Tree
-from aiohttp import TraceRequestChunkSentParams
 import pygame
 from pygame.locals import *
 pygame.init()
+pygame.font.init()
 
 green = (0, 255, 0)
 red = (255, 0, 0)
@@ -12,7 +11,7 @@ red = (255, 0, 0)
 class Snake:
 
     def __init__(self) -> None:
-        self.size = 30
+        self.size = 40
         self.speed = 10   
         self.x = screen.get_width() / 2
         self.y = screen.get_height() / 2
@@ -27,7 +26,7 @@ class Snake:
 
         random_colour = (random.randrange(100, 255), random.randrange(0, 255), random.randrange(0, 255))
 
-        chosen_colour = random_colour if snake.colour == "random" else green
+        chosen_colour = random_colour if self.colour == "random" else green
 
         for x in self.snake_list:
             pygame.draw.rect(screen, chosen_colour, [x[0], x[1], self.size, self.size])
@@ -37,27 +36,52 @@ class Snake:
 class Food:
 
     def __init__(self) -> None:
-        self.x = round(random.randrange(0, screen.get_width() - Snake().size) / 30) * 30
-        self.y = round(random.randrange(0, screen.get_height() - Snake().size) / 30) * 30
+        self.x = round(random.randrange(0, screen.get_width() - Snake().size) / 40) * 40
+        self.y = round(random.randrange(0, screen.get_height() - Snake().size) / 40) * 40
 
     def draw_food(self):
-        pygame.draw.rect(screen, red, [self.x, self.y, 30, 30])
+        pygame.draw.rect(screen, red, [self.x, self.y, 40, 40])
 
     def move(self):
-        self.x = round(random.randrange(0, screen.get_width() - Snake().size) / 30) * 30
-        self.y = round(random.randrange(0, screen.get_height() - Snake().size) / 30) * 30
+        self.x = round(random.randrange(0, screen.get_width() - Snake().size) / 40) * 40
+        self.y = round(random.randrange(0, screen.get_height() - Snake().size) / 40) * 40
 
 
 clock = pygame.time.Clock()
-screen = pygame.display.set_mode((480, 480))
+screen = pygame.display.set_mode((640, 640))
 pygame.display.set_caption("Snake Game")
-snake = Snake()
-food = Food()
+pygame.display.set_icon(pygame.image.load("art/SnakeHead.png"))
 
+score = 0
+
+def game_close():
+    scorefont = pygame.font.SysFont("comicsansms", 35)
+    text = scorefont.render(f"Score: {score}", True, red)
+    text2 = scorefont.render(f"Exit - ESC, Restart - ENTER", True, red)
+    gameover = pygame.image.load("art/GameOver.png")
+    screen.fill((0, 0, 0))
+    while True: 
+        screen.blit(gameover, (50, screen.get_height() - 600))
+        screen.blit(text, (50, screen.get_height() - 435))
+        screen.blit(text2, (50, screen.get_height() - 370))
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                if event.key == pygame.K_RETURN:
+                    main()
 
 
 def main():
+    global score
+
+    snake = Snake()
+    food = Food()
     game_over = False
+    snake.x = screen.get_width() / 2
+    snake.y = screen.get_height() / 2
 
     while not game_over:
 
@@ -82,7 +106,7 @@ def main():
                     snake.colour = "random" if snake.colour == "black" else "black"
 
         if snake.x >= screen.get_width() or snake.x < 0 or snake.y >= screen.get_height() or snake.y < 0:
-            game_over = True
+            game_close()
 
         snake.x += snake.x_change
         snake.y += snake.y_change
@@ -97,7 +121,7 @@ def main():
 
         for x in snake.snake_list[:-1]:
             if x == snake.head:
-                game_over = True
+                game_close()
 
         
         snake.draw_snake()
@@ -107,6 +131,7 @@ def main():
         if snake.x == food.x and snake.y == food.y:
             food.move()
             snake.length += 1
+            score += 1
 
         clock.tick(snake.speed)
 

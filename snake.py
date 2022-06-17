2b3cbc40536cfe49704b2dc40ed199bf93fa3e60
Stabowleo <minegineer71@gmail.com>
2022-06-17 19:16:01 +0100

    Changing code

---
+++ snake.py
@@ -0,0 +1,17 @@
+from jinja2 import pass_context
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+screen = pygame.display.set_mode(480, 480)
+
+
+
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
